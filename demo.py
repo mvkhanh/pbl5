cf845c763566f8785f68968fcc4581311c3458e5
mvk <mvkhanh100204@gmail.com>
2025-03-12 15:22:18 +0700

    .

---
+++ demo.py
@@ -0,0 +1,28 @@
+from dataset import get_dataloader
+import torch
+import torch.nn as nn
+from model import get_model
+import os
+import numpy as np
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {DEVICE}")
+CHECKPOINT_PATH = "ckpt/best_model.pth"
+
+test_normal_path = 'UniformerData/Test/NormalVideos/'
+test_abnormal_path = 'UniformerData/Test/Abnormal/'
+
+def load_checkpoint(model, checkpoint_path):
+        """Nạp lại trạng thái từ checkpoint."""
+        checkpoint = torch.load(checkpoint_path, map_location=DEVICE)
+        model.load_state_dict(checkpoint['model_state'])
+        print(f"🔄 Load successfully!")
+
+if __name__ == '__main__':
+    # Model
+    model = get_model().to(DEVICE)
+
+    if os.path.exists(CHECKPOINT_PATH):
+         load_checkpoint(model, CHECKPOINT_PATH)
+
+    video_path = os.path.join()
