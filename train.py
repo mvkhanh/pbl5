e3f8bd3c89d67010f1187e62537b59822d06ae41
mvk <mvkhanh100204@gmail.com>
2025-03-13 09:43:10 +0700

    .

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     model = get_model().to(DEVICE)
 
     # Train
-    pos_weight = torch.tensor([35000 / 7500])  # Tăng trọng số lớp dương
+    pos_weight = torch.tensor([2.5])  # Tăng trọng số lớp dương
     loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight.to(DEVICE))
     trainer = Trainer(model, train_loader, test_loader, loss_fn,
                   optim.Adam(params=model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY), epochs=EPOCHS, device=DEVICE, 
