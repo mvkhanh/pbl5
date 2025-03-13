75ebd71db08326284a08631faeecfebe958f8aea
mvk <mvkhanh100204@gmail.com>
2025-03-13 12:36:28 +0700

    .

--- collect_pr_rc.py
+++ collect_pr_rc.py
@@ -20,43 +20,6 @@ train_normal_path = 'UniformerData/Train/NormalVideos/'
 test_normal_path = 'UniformerData/Test/NormalVideos/'
 test_abnormal_path = 'UniformerData/Test/Abnormal/'
 
-def eval1(model, loss_fn, data_loader):
-    """Đánh giá mô hình trên tập test và tính Precision, Recall, F1-score."""
-    model.eval()
-    total_loss = 0
-    all_preds = []
-    all_labels = []
-    
-    with torch.inference_mode(), torch.autocast(device_type=DEVICE, dtype=torch.float16):  # ✅ Dùng float16
-        for inputs, labels in data_loader:
-            inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
-            
-            outputs = model(inputs)  # Forward pass
-            
-            # Tính loss
-            loss = loss_fn(outputs, labels)
-            total_loss += loss.item()
-            outputs = torch.sigmoid(outputs)
-            # Chuyển output thành nhãn dự đoán (0 hoặc 1)
-            preds = (outputs > 0.5).float()
-            
-            # Lưu lại dự đoán và nhãn thật
-            all_preds.extend(preds.cpu().numpy())
-            all_labels.extend(labels.cpu().numpy())
-
-    # Chuyển về numpy array
-    all_preds = np.array(all_preds).flatten()
-    all_labels = np.array(all_labels).flatten()
-
-    # Tính các chỉ số đánh giá
-    precision = precision_score(all_labels, all_preds)
-    recall = recall_score(all_labels, all_preds)
-    f1 = f1_score(all_labels, all_preds)
-    acc = (all_preds == all_labels).mean() 
-
-    avg_loss = total_loss / len(data_loader)
-
-    return avg_loss, acc, precision, recall, f1
 
 def load_checkpoint(model, checkpoint_path):
         """Nạp lại trạng thái từ checkpoint."""
