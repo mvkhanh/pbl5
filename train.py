21a2ffcf0098130dfd534af2a6f8f4fe66dfb507
mvk <mvkhanh100204@gmail.com>
2025-03-26 14:33:26 +0700

    .

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import os
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {DEVICE}")
 EPOCHS = 100
-LEARNING_RATE = 1e-3
+LEARNING_RATE = 5e-3
 WEIGHT_DECAY = 0.005
 BATCH_SIZE = 45
 PATIENCE = 7
